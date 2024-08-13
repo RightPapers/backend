68897a4064ade2aff393c8b205fc4df5e10683e1
2uwls <2uwls1126@gmail.com>
2024-08-13 22:25:42 +0900

    fix: modify config

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ roberta_model_path = os.path.join(model_base_path, 'reberta_ACC_0.9265.pth')
 transformer_model_path = os.path.join(model_base_path, 'transformer_ACC_0.9231.pth')
 
 #데이터 베이스 설정
-'''
+
 DATABASE_URI = os.getenv('DATABASE_URI')
 if not DATABASE_URI:
     raise ValueError("DATABASE_URI 환경 변수가 설정되지 않았습니다.")
@@ -53,7 +53,7 @@ except Exception as e:
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-'''
+
 
 # JSON 파일 저장 경로 설정
 json_save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'saved_data')
@@ -61,7 +61,7 @@ json_save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'saved
 # 디렉토리가 존재하지 않으면 생성
 if not os.path.exists(json_save_path):
     os.makedirs(json_save_path)
-'''
+
 db = SQLAlchemy(app)
 class Feedback(db.Model):
     __tablename__ = 'FEEDBACK'
@@ -73,7 +73,7 @@ class Feedback(db.Model):
 
     def __repr__(self):
         return f'<Feedback {self.feedback_id}>'
-'''
+
 def analyze_video(url):
     # Use YouTubeCaptionCrawler to get video details and captions
     crawler = YouTubeCaptionCrawler(url)
--- config.py
+++ config.py
@@ -1,19 +1,25 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 class Config:
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     MODEL_PATH = os.environ.get('MODEL_PATH', 'model/your_model.h5')
     
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     MODEL_PATH = os.path.join(BASE_DIR, 'AI', 'models')
 
+
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 
 class TestingConfig(Config):
     TESTING = True
